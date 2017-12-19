2f94465e5a787cd6a20fbaf482fd7fbd83a04668
Diana <diana.smolinsska@gmail.com>
2017-12-19 21:35:31 +0200

    MainPage

---
+++ smolinskaiaProj/src/main/java/pages/MainPage.java
@@ -0,0 +1,20 @@
+package pages;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+
+public class MainPage {
+    WebDriver webDriver;
+
+    public MainPage(WebDriver webdriver) {
+        this.webDriver = webdriver;
+    }
+
+    public boolean isAvatarPresent() {
+        try {
+           return webDriver.findElement(By.xpath(".//div[@class='pull-left image']//img[@class='img-circle']")).isDisplayed();
+        } catch (Exception e) {
+            return false;
+        }
+    }
+}
