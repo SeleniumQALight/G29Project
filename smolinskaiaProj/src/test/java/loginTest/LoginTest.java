6ba20aa04a3dc1f2de818695eced1879660196d3
Diana <diana.smolinsska@gmail.com>
2017-12-21 21:37:18 +0200

    ParentClass

--- smolinskaiaProj/src/test/java/loginTest/LoginTest.java
+++ smolinskaiaProj/src/test/java/loginTest/LoginTest.java
@@ -3,19 +3,17 @@ package loginTest;
 import org.junit.Assert;
 import org.junit.Test;
 import org.openqa.selenium.By;
-import parentTest.ParentClass;
+import parentTest.ParentTest;
 
-import java.util.concurrent.TimeUnit;
-
-public class LoginTest extends ParentClass {
+public class LoginTest extends ParentTest {
 
     @Test //этот тест может быть запущен (JUnit с аннотацией)
     public void validLogin() {
 
-        webDriver.manage().window().maximize();
-        webDriver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);//пробуй каждые полсекунды сделать это на протяжении 10 секунд
-        webDriver.get("http://v3.test.itpmgroup.com");
-        webDriver.findElement(By.name("_username")).sendKeys("Student");
+       // webDriver.get("http://v3.test.itpmgroup.com");
+       // webDriver.findElement(By.name("_username")).sendKeys("Student");
+        loginPage.openLoginPage();
+        loginPage.enterTextIntoInputLogin("Student");
         webDriver.findElement(By.id("password")).sendKeys("909090");
         webDriver.findElement(By.xpath(".//button[@type='submit']")).click();
 
