38a84eeab0384f8324876300c6339327cd032645
Diana <diana.smolinsska@gmail.com>
2017-12-21 21:37:32 +0200

    ParentClass

--- smolinskaiaProj/src/main/java/pages/MainPage.java
+++ smolinskaiaProj/src/main/java/pages/MainPage.java
@@ -3,14 +3,16 @@ package pages;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 
-public class MainPage {
-    WebDriver webDriver;
+public class MainPage extends ParentPage{
 
-    public MainPage(WebDriver webdriver) {
-        this.webDriver = webdriver;
+       public MainPage(WebDriver webDriver) {
+       super(webDriver);
     }
 
     public boolean isAvatarPresent() {
+        logger.info("Test");
+        logger.error("Error");
+        logger.debug("Debug");
         try {
            return webDriver.findElement(By.xpath(".//div[@class='pull-left image']//img[@class='img-circle']")).isDisplayed();
         } catch (Exception e) {
