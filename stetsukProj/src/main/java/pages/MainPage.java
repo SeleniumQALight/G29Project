214cf40d0a3083b96ad881a828dcb12d404b77b7
serhii <sergeystetsiuk@gmail.com>
2018-01-09 19:29:11 +0200

    General Push

--- stetsukProj/src/main/java/pages/MainPage.java
+++ stetsukProj/src/main/java/pages/MainPage.java
@@ -1,4 +1,22 @@
 package pages;
 
-public class mainPage {
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+
+public class MainPage extends ParentPage {
+    public MainPage(WebDriver webDriver) {
+        super(webDriver);
+    }
+
+    public boolean isAvatarPresent() {
+        logger.info("Test");
+        logger.error("Error");
+        logger.debug("Debug");
+        try {
+            return webDriver.findElement(By.xpath(".//div[@class='pull-left image']//img[@class='img-circle']")).isDisplayed();
+        } catch (Exception e) {
+            return false;
+        }
+    }
+
 }
