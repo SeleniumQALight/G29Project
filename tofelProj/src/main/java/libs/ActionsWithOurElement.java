983963e4ff8b04b0c16fc3188c96ec0a9cf6afde
Margo <marinatofel@gmail.com>
2018-01-09 21:39:21 +0200

    actions with our elements was added

---
+++ tofelProj/src/main/java/libs/ActionsWithOurElement.java
@@ -0,0 +1,59 @@
+package libs;
+
+import org.apache.log4j.Logger;
+import org.junit.Assert;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+
+
+public class ActionsWithOurElement {
+    WebDriver webDriver;
+
+
+    Logger logger;
+
+            public ActionsWithOurElement(WebDriver webDriver){
+        this.webDriver=webDriver;
+        logger= Logger.getLogger(getClass());
+            }
+
+    /**
+     * Method Enter text in to input and textArea
+     * @param input
+     * @param text
+     */
+    public void enterTextInToInput(WebElement input,String text){
+                try {
+                    input.clear();
+                    input.sendKeys(text);
+                    logger.info(text +"was inputed in to input" + input);
+                }catch (Exception e){
+                    logErrorAndStopTest(input);
+
+                }
+            }
+
+            public void clickOnElement(WebElement element){
+                try{
+                    element.click();
+                    logger.info("Element was clicked" + element);
+                }catch(Exception e){
+                    logErrorAndStopTest(element);
+                }
+            }
+
+            public boolean isElementPresent(WebElement element){
+                try {
+                    boolean tempState=element.isDisplayed()&&element.isEnabled();
+                    logger.info("Is Element present ? -"+ tempState);
+                    return tempState;
+                }catch (Exception e){
+                    logger.info("Is Element present ? -"+ false);
+                    return false;
+                }
+            };
+            private void logErrorAndStopTest(WebElement element){
+                logger.error("Can not work with element" + element);
+                        Assert.fail("Can not work with element"+ element);
+            }
+}
