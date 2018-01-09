8d0a905924c92d42a3197c525f3fe4fc24e57296
mliubchenko <marina.amado11@gmail.com>
2018-01-09 21:26:32 +0200

    ActionWithOutElement was added

---
+++ lubchenkoProj/src/main/java/libs/ActionsWithOurElement.java
@@ -0,0 +1,50 @@
+package libs;
+
+import org.apache.log4j.Logger;
+import org.junit.Assert;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+
+public class ActionsWithOurElement {
+    WebDriver webDriver;
+    Logger logger;
+
+    public ActionsWithOurElement(WebDriver webDriver) {
+        this.webDriver = webDriver;
+        logger = Logger.getLogger(getClass ());
+    }
+
+    public void enterTextInToInput (WebElement input, String text){
+        try {
+            input.clear();
+            input.sendKeys(text);
+            logger.info(text + " was inputed in to input " + input);
+        } catch (Exception e){
+            logErrorAndStopTest(input);
+        }
+    }
+
+    public void clickOnElement (WebElement element) {
+        try {
+            element.click ();
+            logger.info ( "Element was clicked " + element);
+        }catch (Exception e){
+            logErrorAndStopTest(element);
+        }
+    }
+
+    public boolean isElementPresent (WebElement element) {
+        try {
+            boolean tempState = element.isDisplayed()&& element.isEnabled();
+            logger.info("Is Element Present ? - " + tempState);
+            return tempState;
+        }catch (Exception e){
+            logger.info("Is Element Present ? -false");
+            return false;
+        }
+    }
+    private void logErrorAndStopTest (WebElement element) {
+        logger.error ("Can not work with element " + element);
+        Assert.fail ("Can not work with element " + element);
+    }
+}
