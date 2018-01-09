039a35a1d021350ee34a77e2e11050650b104bfd
QA Teahcer <radulenko@gmail.com>
2018-01-09 21:25:47 +0200

    actionsWithOurElements was added

--- overAll/src/main/java/pages/MainPage.java
+++ overAll/src/main/java/pages/MainPage.java
@@ -11,9 +11,6 @@ public class MainPage extends ParentPage{
     }
 
     public boolean isAvatarPresent(){
-//        logger.info("Test");
-//        logger.error("Error");
-//        logger.debug("Debug");
         try{
             return webDriver.findElement(
                     By.xpath(".//div[@class='pull-left image']//img[@class='img-circle']"))
