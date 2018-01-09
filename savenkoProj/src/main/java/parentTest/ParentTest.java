93e73f37be742f5bade7f1e68375789783c496c2
sliudmyla <mila.savenko@gmail.com>
2018-01-09 19:38:50 +0200

    commit last lesson

---
+++ savenkoProj/src/main/java/parentTest/ParentTest.java
@@ -0,0 +1,38 @@
+package parentTest;
+
+import org.junit.After;
+import org.junit.Before;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import pages.LoginPage;
+import pages.MainPages;
+
+
+
+import java.io.File;
+import java.util.concurrent.TimeUnit;
+
+public class ParentTest {
+    public WebDriver webDriver;
+    protected MainPages mainPage;                                       //доступний тільки у наслідника у всіх папках
+    protected LoginPage loginPage;
+
+    @Before                                                           // це Пре кондішн, відкриває браузер ..Анотація дж-юніта. Виконується перед кожним тесткейсом
+
+    public void setUp() {
+        File fileFF = new File("./drivers/chromedriver.exe");         // заімпортили веб драйвер для хрома
+        System.setProperty("webdriver.chrome.driver", fileFF.getAbsolutePath()); // установлюємо системну змінну, щоб знала шлях до файлу
+        webDriver = new ChromeDriver();                               //
+        mainPage = new MainPages(webDriver);                           // ініціалізуємо клас MainPage + Дали браузер
+        loginPage = new LoginPage(webDriver);
+
+
+        webDriver.manage().window().maximize(); //open window full screen
+        webDriver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);    }
+
+    @After
+    public void tearDown() {
+        webDriver.quit(); // Закриє браузер
+    }
+
+}
