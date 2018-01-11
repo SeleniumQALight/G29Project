ea94005a0f5e8a85b5dca906e5fd978a32c894c9
viktoriia.druchenko <viktoriia.druchenko@gmail.com>
2018-01-11 21:37:05 +0200

    refactoring

--- druchenkoProj/src/main/java/pages/ParentPage.java
+++ druchenkoProj/src/main/java/pages/ParentPage.java
@@ -1,7 +1,11 @@
 package pages;
 
+import libs.ActionsWithOurElements;
 import org.apache.log4j.Logger;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.support.PageFactory;
+
+import static libs.ActionsWithOurElements.*;
 
 public class ParentPage {
     WebDriver webDriver;
@@ -9,7 +13,10 @@ public class ParentPage {
     protected Logger logger;
 
     public ParentPage(WebDriver webDriver){  //конструктор не возвращает значения и называется как класс (создается с помощью Alt + insert)
+        ActionsWithOurElements actionsWithOurElements;
         this.webDriver = webDriver;
         logger = Logger.getLogger(getClass());
+        PageFactory.initElements(webDriver, this);
+        actionsWithOurElements = new ActionsWithOurElements(webDriver);
     }
 }
