b906ea24e5a337e1bb4363f13f4aa7b54bdd38f5
Diana <diana.smolinsska@gmail.com>
2018-01-11 21:37:07 +0200

    all_files

--- smolinskaiaProj/src/main/java/pages/MainPage.java
+++ smolinskaiaProj/src/main/java/pages/MainPage.java
@@ -1,20 +1,31 @@
 package pages;
 
+import libs.ActionsWithOurElements;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import static libs.ActionsWithOurElements.*;
 
 public class MainPage extends ParentPage{
-
+    @FindBy(xpath = ".//div[@class='pull-left image']//img[@class='img-circle']")
+    private WebElement avatar;
+    @FindBy (id = "dictionary")
+    private WebElement menuDictionary;
+    @FindBy (id = "spares")
+    private WebElement subMenuSpares;
        public MainPage(WebDriver webDriver) {
        super(webDriver);
     }
 
     public boolean isAvatarPresent() {
+    return isElementPresent(avatar);
+       }
 
-        try {
-           return webDriver.findElement(By.xpath(".//div[@class='pull-left image']//img[@class='img-circle']")).isDisplayed();
-        } catch (Exception e) {
-            return false;
-        }
+       public void clickOnMenuDictionary (){
+           clickOnElement(menuDictionary);
+                  }
+    public void clickOnMenuSpare (){
+           clickOnElement(subMenuSpares);
     }
 }
