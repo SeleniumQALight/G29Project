6343a4ab52770213c58cbb53a8ffd668f6edf144
MykolaKozak <kozakmykola1@gmail.com>
2018-01-11 21:38:08 +0200

    Refactor

--- kozakProj/src/main/java/pages/ParentPage.java
+++ kozakProj/src/main/java/pages/ParentPage.java
@@ -1,18 +1,23 @@
 package pages;
 
-        import org.apache.log4j.Logger;
-        import org.openqa.selenium.WebDriver;
+import libs.ActionsWithOurElement;
+import org.apache.log4j.Logger;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.support.PageFactory;
+
+import static libs.ActionsWithOurElement.*;  //заімпортили вручну лібс і її методи
 
 public class ParentPage {
     WebDriver webDriver;
+    ActionsWithOurElement actionsWithOurElement;
     protected Logger logger;
 
-    public ParentPage (WebDriver webDriver){
+    public ParentPage(WebDriver webDriver) {
         this.webDriver = webDriver;
         logger = Logger.getLogger(getClass());
+        PageFactory.initElements(webDriver,this);
+        actionsWithOurElement = new ActionsWithOurElement(webDriver);
     }
-
-
 
 
 }
