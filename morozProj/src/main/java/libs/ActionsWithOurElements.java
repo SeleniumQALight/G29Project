3fcb56a2b5fb64dec6826191685d655d3fec9e5b
DESKTOP-KEMQAHD\NATA <fenita911@gmail.com>
2018-01-23 15:00:57 +0200

    new spare test was added

--- morozProj/src/main/java/libs/ActionsWithOurElements.java
+++ morozProj/src/main/java/libs/ActionsWithOurElements.java
@@ -4,14 +4,19 @@ import org.apache.log4j.Logger;
 import org.junit.Assert;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.Select;
+import org.openqa.selenium.support.ui.WebDriverWait;
 
 public class ActionsWithOurElements {
     WebDriver webDriver;
     static Logger logger;
+    static WebDriverWait webDriverWait20;
 
     public ActionsWithOurElements(WebDriver webDriver) {
         this.webDriver = webDriver;
         logger = Logger.getLogger("ActionsWithOurElements");
+        webDriverWait20 = new WebDriverWait(webDriver, 20);
     }
     public static void enterTextInToInput(WebElement input, String text){
         try {
@@ -21,9 +26,12 @@ public class ActionsWithOurElements {
         }catch (Exception e){
             logErrorAndStopTest();
         }
+
     }
     public static void clickOnElement(WebElement element) {
         try{
+            webDriverWait20.until(ExpectedConditions.elementToBeClickable(element));
+            //webDriverWait20.until(ExpectedConditions.not(ExpectedConditions.invisibilityOf(element)));
             element.click();
             logger.info("Element was clicked " + element);
         }catch (Exception e){
@@ -44,6 +52,18 @@ public class ActionsWithOurElements {
     private static void logErrorAndStopTest(){
         logger.error("Can not work with element ");
         Assert.fail("Can not work with element " );
+    }
+
+    public void selectOptionsInDropDown(WebElement selectDropDown, String textInDropDown){
+        try {
+            Select options = new Select(selectDropDown);
+            options.selectByVisibleText(textInDropDown);
+            logger.info(textInDropDown + " was selected in DD");
+
+        }catch(Exception e){
+            logErrorAndStopTest();
+        }
+
     }
 
 }
