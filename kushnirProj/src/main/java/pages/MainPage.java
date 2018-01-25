5dbed3fc7cf87e5cf3a87232f12485d698cabdc3
mkushnir8 <mkushnir8@gmail.com>
2018-01-25 12:44:58 +0200

    commit

--- kushnirProj/src/main/java/pages/MainPage.java
+++ kushnirProj/src/main/java/pages/MainPage.java
@@ -1,31 +1,35 @@
 package pages;
 
 
-import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
-//если мы хотим найти аватарку на мейнпейдже, значит мы его разрабатываем
-//если есть класс, описывающий пейджу, описываем пейджу
-//если нет, значит нет нигде
-public class MainPage extends ParentPage {
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
 
+import static libs.ActionsWithOurElements.*;
+
+public class MainPage extends ParentPage{
+    @FindBy(xpath = ".//div[@class='pull-left image']//img[@class='img-circle']")
+    private WebElement avatar;
+
+    @FindBy(id = "dictionary")
+    private WebElement menuDictionary;
+
+    @FindBy(id = "spares")
+    private WebElement subMenuSpares;
 
     public MainPage(WebDriver webDriver) {
-        super(webDriver);
+        super(webDriver,"/");
     }
 
-
-
-    public boolean isAvatarPresent() {
-        logger.info("Test");
-        logger.error("Error");
-        logger.debug("Debug");
-        try {
-            return webDriver.findElement(
-                    By.xpath(".//div[@class='pull-left image']//img[@class='img-circle']")).isDisplayed();//если експешн - викинет катч
-
-        } catch (Exception e) {
-            return false;
-        }
+    public boolean isAvatarPresent(){
+       return isElementPresent(avatar);
     }
 
+    public void clickOnMenuDictionary(){
+        clickOnElement(menuDictionary);
+    }
+
+    public void clickOnSubMenuSpare(){
+        clickOnElement(subMenuSpares);
+    }
 }
--- kushnirProj/src/test/java/parentTest/ParentTest.java
+++ kushnirProj/src/test/java/parentTest/ParentTest.java
@@ -4,14 +4,20 @@ import org.junit.After;
 import org.junit.Before;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.ie.InternetExplorerDriver;
+import org.openqa.selenium.remote.CapabilityType;
+import org.openqa.selenium.remote.DesiredCapabilities;
 import pages.LoginPage;
 import pages.MainPage;
+import org.apache.log4j.Logger;
 
 import java.io.File;
 import java.util.concurrent.TimeUnit;
-
+import org.junit.Assert;
 public class ParentTest {
     public WebDriver webDriver;
+    String browser = System.getProperty("browser");
+    Logger log;
     protected MainPage mainPage;
     protected LoginPage loginPage;
 
@@ -20,13 +26,33 @@ public class ParentTest {
 
     @Before
     public void setUp() {
-        File fileFF = new File("./drivers/chromedriver.exe");
-        System.setProperty("webdriver.chrome.driver", fileFF.getAbsolutePath());
-        webDriver = new ChromeDriver();//метод нужно выполнить раньше
+        log = Logger.getLogger(getClass());
+        setBrowser();
         webDriver.manage().window().maximize();//открыли на весь экран. скажи менеджеру повлиять на окошко
         webDriver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);//время, на протяжении которого будет пытаться что-то сделать
         mainPage = new MainPage(webDriver);
         loginPage = new LoginPage(webDriver);
+    }
+
+    private void setBrowser() {
+        if ("chrome".equals(browser)) {
+
+            File fileFF = new File("./drivers/chromedriver.exe");
+            System.setProperty("webdriver.chrome.driver", fileFF.getAbsolutePath());
+            webDriver = new ChromeDriver();//метод нужно выполнить раньше
+        }else if ("ie".equals(browser)){
+            log.info("IE will be started");
+            File file1 = new File(".././drivers/IEDriverServer.exe");
+            System.setProperty("webdriver.ie.driver", file1.getAbsolutePath());
+            DesiredCapabilities capabilities = DesiredCapabilities.internetExplorer();
+            capabilities.setCapability(InternetExplorerDriver.INTRODUCE_FLAKINESS_BY_IGNORING_SECURITY_DOMAINS, true);
+            capabilities.setCapability("ignoreZoomSetting", true);
+            capabilities.setCapability(CapabilityType.ACCEPT_SSL_CERTS, true);
+            webDriver = new InternetExplorerDriver();
+            log.info(" IE is started");
+        }else {
+            Assert.fail("Can not open browser"+browser);
+        }
     }
 
     @After
